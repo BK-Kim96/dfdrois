9c5133f0ed14676f1c48b2235e6facb2edcef8e9
BK-Kim96 <67535169+BK-Kim96@users.noreply.github.com>
2024-04-02 19:27:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,45 +2,20 @@
 
 import pickle
 import streamlit as st
-from sklearn.ensemble import RandomForestClassifier
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
 
 # loading the trained model
 pickle_in = open('rf.pkl', 'rb')
 classifier = pickle.load(pickle_in)
 
-
 @st.cache()
 
 def prediction(temperature,bpm):
-    input_data = [[bpm, temperature]]
-    prediction=classifier.predict(input_data)
+    prediction=rf.predict(temperature,bpm)
     return prediction
 
 
 def main():
     # front end elements of the web page
-    app = Flask(__name__)
-    api = Api(app)
-    
-    class RegistUser(Resource):
-        def post(self):
-           
-            parser = reqparse.RequestParser()
-            parser.add_argument('TEMP', type=float)
-            parser.add_argument('HR', type=float)
-            parser.add_argument('RMSSD', type=float)
-            args = parser.parse_args()
-            temp = args['TEMP']
-            hr = args['HR']
-            rmssd = args['RMSSD']
-            emotion = prediction(temp,hr)
-            return {'predict emotion': emotion}
-    
-    api.add_resource(RegistUser, '/emotion')
-
-
     html_temp = """ 
     <div style ="background-color:yellow;padding:13px"> 
     <h1 style ="color:black;text-align:center;">Streamlit Loan Prediction ML App</h1> 
@@ -58,5 +33,5 @@ def main():
         result = prediction(temperature, bpm)
         st.success('Your emotion is {}'.format(result))
 
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
